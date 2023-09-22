9d88a9c0b2d603450b1216723f718273ab400b21
emaile <sanya.volkov344.volkov@mail.ru>
2023-09-23 02:50:36 +0300

    ready

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { Component, NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ItemComponent } from './item/item.component';
+import { DetailsComponent } from './item/details/details.component';
+import { ListComponent } from './item/list/list.component';
+
+const routes: Routes = [
+  { path:'about', component: AboutComponent},
+  { path:'contact', component: ContactComponent},
+  {
+    path: 'item/:id',
+    component: ItemComponent,
+    children: [
+      { path: 'details', component: DetailsComponent },
+      { path: 'list', component: ListComponent},
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ItemComponent } from './item/item.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DetailsComponent } from './item/details/details.component';
+import { ListComponent } from './item/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AboutComponent,
+    ItemComponent,
+    DetailsComponent,
+    ListComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
