d32e073e8b8256e583459c046b7d072b6cd84e5d
emaile <sanya.volkov344.volkov@mail.ru>
2023-10-12 10:14:17 +0300

    Ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SecondComponent } from './second/second.component';
+import { ResolveService } from './resolve.service';
+import { DataService } from './data.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ResolveService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+
+    constructor(private http: HttpClient) {}
+
+    fetchPosts(): Observable<any> {
+        return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    }
+
+    fetchComments(postId: number): Observable<any> {
+        return this.http.get('https://jsonplaceholder.typicode.com/comments', { params: { postId: postId.toString() } });
+    }
+
+    createPost(): Observable<any> {
+        return this.http.post('https://jsonplaceholder.typicode.com/posts', {});
+    }
+
+    getWithErrorHandling(): Observable<any> {
+        return this.http.get('https://jsonplaceholder.typicode.com/post').pipe();
+    }
+
+    getWithHeaders(): Observable<any> {
+        const headers = { 'X-Test': '1' };
+        return this.http.get('https://jsonplaceholder.typicode.com/posts', { headers, responseType: 'text' });
+    }
+
+    deletePost(): Observable<any> {
+        return this.http.delete('https://jsonplaceholder.typicode.com/posts/1');
+    }
+}
